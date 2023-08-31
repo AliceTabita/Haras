073b31da40644efc620d64156210fe5abc037b3e
AliceTabita <alicetabita26@gmail.com>
2023-08-31 03:40:34 -0300

    Adicionando main

---
+++ src/main/java/br/com/haras/Main.java
@@ -0,0 +1,11 @@
+package br.com.haras;
+import br.com.haras.view.proprietarios.FrProprietarios;
+
+public class Main
+{
+    public static void main(String[] args) {
+        //new FrInicio();
+        new FrProprietarios();
+        //new FrEquinos();
+    }
+}
