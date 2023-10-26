485904c8130dd77fdc7ebf7666069e8aff350c12
alice <alicetabita26@gmail.com>
2023-10-26 03:28:57 -0300

    iniciando formulario menu na main

--- src/main/java/br/com/haras/Main.java
+++ src/main/java/br/com/haras/Main.java
@@ -1,11 +1,20 @@
 package br.com.haras;
-import br.com.haras.view.proprietarios.FrProprietarios;
+import br.com.haras.model.Cliente;
+import br.com.haras.model.dao.ClienteDao;
+import br.com.haras.view.ClienteGUI;
+import br.com.haras.view.FrMenu;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
 
 public class Main
 {
     public static void main(String[] args) {
-        //new FrInicio();
-        new FrProprietarios();
-        //new FrEquinos();
+        new FrMenu();
+        
+
     }
 }
