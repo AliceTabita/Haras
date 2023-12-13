850dfd08c3e73dae0a55c336a01c5bd0e87281bf
alice <alicetabita26@gmail.com>
2023-12-13 20:29:34 -0300

    add combobox

---
+++ src/main/java/br/com/haras/view/component/swing/SearchableComboBox.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.haras.view.component.swing;
+
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author alice
+ */
+public class SearchableComboBox<E>  extends JComboBox<E>{
+    
+    public SearchableComboBox( ) {
+        setUI(new ComboBoxSuggestionUI());       
+    }
+}
